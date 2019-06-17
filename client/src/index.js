e83569b129b72334353b2ced1b545df73aa474f2
Shaina Lowenthal <shaina.lowenthal96@myhunter.cuny.edu>
2019-06-17 13:36:23 -0400

    added components and store

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 import App from './App';
 import { Provider } from 'react-redux';
-
 import * as serviceWorker from './registerServiceWorker';
-import store from '../src/store/index.js';
+import store from '../src/store/index.js'; //'./store'
 
 
 ReactDOM.render(
