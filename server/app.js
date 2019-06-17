b27eb03e2ca89496b55d1c540d6a75557f0a1529
anupamdas104 <anupamdas104@gmail.com>
2019-06-17 14:19:32 -0400

    add postgres database to the express server

--- server/app.js
+++ server/app.js
@@ -2,6 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const Sequelize = require('sequelize');
+const db = new Sequelize('travelClan', 'postgres', 'password123',{
+    host: 'localhost',
+    port: 5433,
+    dialect: 'postgres'
+});
+
+
+db.authenticate()
+    .then(()=>console.log("DB connected"))
+    .catch(err => console.log('Error is: '+ err))
+
 const app = express();
 
 app.get('/', (req, res) => res.send('INDEX'));
