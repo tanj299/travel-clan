837cbd5acb378c337e05993bc2097099a5b040b1
anupamdas104 <anupamdas104@gmail.com>
2019-06-18 07:22:07 -0400

    add database models for users and trips

--- server/app.js
+++ server/app.js
@@ -1,23 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-
-const Sequelize = require('sequelize');
-const db = new Sequelize('travelClan', 'postgres', 'password123',{
-    host: 'localhost',
-    port: 5433,
-    dialect: 'postgres'
-});
-
-
-db.authenticate()
-    .then(()=>console.log("DB connected"))
-    .catch(err => console.log('Error is: '+ err))
+const database = require('./models')
 
 const app = express();
+database.sequelize.sync({force: true})
 
 app.get('/', (req, res) => res.send('INDEX'));
 
 const PORT = 8080;
 
+//start server
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
