10d1cd6351a711ef0c31648f99870ba86700ed3d
Jayson Tan <tanj299@gmail.com>
2019-06-21 12:06:48 -0400

    Update SingleTrip.js

--- client/src/components/SingleTrip.js
+++ client/src/components/SingleTrip.js
@@ -24,6 +24,13 @@ class SingleTrip extends Component{
     // console.log("this is single trip",this.props.fetchSingleTrip());
   }
 
+
+  handleCity = (event) => {
+    this.setState({
+      city: event.target.value
+    })
+  }
+
   handleLatitude = (event) => {
     this.setState({
       latitude: event.target.value
@@ -36,56 +43,59 @@ class SingleTrip extends Component{
     })
   }
 
+
   // Returns a list of airports and cities matching a given keyword.
-  handleSubmit(trip){
-    // event.preventDefault(); // prevent default refresh
-    console.log("FUN TRIP!!", trip)
-    this.props.fetchAirportsInCity(trip);
+  handleSubmit = (event) => {
+    event.preventDefault(); // prevent default refresh
+    let currentCity = this.props.fetchAirportsInCity(this.state.city);
+  
+    // console.log("Points of Interest")  
     this.props.fetchPointsOfInterest(this.state.latitude, this.state.longitude);
+    // console.log("does this print", currentCity)
   }
   
   render() {
     console.log('Trip Render', this.props)
-    const { singleTrip } = this.props
+    const { thisTrip, singleTrip } = this.props
     const myTrip = singleTrip || []
     
     return (
       <div>
         <div>
-          {/* <h1 className="title"> Trip: {thisTrip.destination}</h1> */}
-          {/* <button onClick = {this.displayChat}>Chat Here!</button> */}
-          <h1>This is single trip</h1>
-          <p><Link to = "/channels/:channelId">ChatHere!</Link> </p>
+          <p className = "myChatBtn"><Link to = "/channels/:channelId"> Need a Clan? Chat Here!</Link> </p>
           {this.props.singleTrip.id}
         </div>
-     {/* =================================================================== */}
-        <div>{ myTrip.map((trip, index) => {
-          let myDestination = trip.destination // gets New York 
-          let myCity = trip.currentCity	// gets NYC 
-          // console.log('current destination', myDestination);
-          // console.log('current destination', myCity);
-          return (
-            <div>
-              <br></br>	
-              <Link to = '/singletrip'>{trip.destination}</Link>
-              {this.setState({
-                city: trip.destination
-              })}
-            </div>
-          )
-          })
-        }</div>
-      {/*================================================================== */}
+    
         <div>
-          <br/>
-          <form onSubmit= {this.handleSubmit(this.state.city)} >
-            <label> Connect with Airports in: </label>  
-              <input placeholder="39.961388 Latitude" onChange={this.handleLatitude}/>
-              <input placeholder="39.961388 Longitude" onChange={this.handleLongitude}/>
-            <button> Things To Do </button>
-          </form >
-        
+          
+        {/* ================================================================ */}
+          {myTrip.map((trip, index) => {
+            let myDestination = trip.destination // gets New York 
+            let myCity = trip.currentCity	// gets NYC 
 
+            return (
+              <div>
+                <br></br>	
+              </div>)
+            }
+          )} </div>
+
+        {/* ================================================================ */}
+          
+        <div>
+        
+        <form onSubmit= {this.handleSubmit} >
+          <h2 className = "places"> Find Places to Visit </h2>
+          <label className = "login-label"> Enter Coordinates </label>
+            {/* <input type="text" placeholder="Input city i.e. London, Paris, NYC" onChange={this.handleCity}/>
+            <label> Connect with Airports in: </label>   */}
+            <input placeholder="39.961388 Latitude" onChange={this.handleLatitude}/>
+            <input placeholder="39.961388 Longitude" onChange={this.handleLongitude}/>
+            <div className = "button-wrapper">
+               <button className = "myTripBtn"> Find Places to Visit </button>
+            </div>
+        </form >
+        
         {/* Map over All Airports In Designated City */}
         <div>{
           this.props.airportsInCity.map(res => {
@@ -95,21 +105,41 @@ class SingleTrip extends Component{
             </div> );
           }) 
         }</div>
+          {/* Map over Points of Interest */}
+          <div>{
+            this.props.pointsOfInterest.map(res => {
+              console.log(res);
+              return (
+                <div className="pointsOfInterest">
+                  <div> {res.address.cityName}, {res.address.countryName} </div>
 
-        
+                  <div> Type: {res.name} </div>
+                  <div> Traveler's Score {
+                    res.analytics.travelers.score > 5 ? <span id="recommend"> RECOMMENDED </span> :
+                      <span id="notRecommend"> NOT RECOMMENDED </span>
+                  }</div>
+                  <div> Flight's Score {
+                    res.analytics.flights.score > 5 ? <span id="recommend"> RECOMMENDED </span> :
+                      <span id="notRecommend"> NOT RECOMMENDED </span>
+                  }</div>
+                </div>);
+            })
+          }</div>
         {/* Map over Points of Interest */}
-        <div>{
+        {/* <div>{
           this.props.pointsOfInterest.map(res => {
             return (
-              <div> 
-                <div> Points Of Interest: { res.name } </div> 
-                <div> Latitude: { res.geoCode.latitude } </div> 
-                <div> Longitude: { res.geoCode.longitude } </div> 
-                <div> Description: { res.category } </div> 
-                
-              </div> );
+              <div className = "login-form">
+              <div className = "edit-form"> 
+                <p> Points Of Interest: { res.name } </p> 
+                <p> Latitude: { res.geoCode.latitude } </p> 
+                <p> Longitude: { res.geoCode.longitude } </p> 
+                <p> Should you go: { res.analytics.travelers.score < 5 ? <span className="notRecommended"> Not Recommended </span> : <span className = "recommended"> Yes </span> }</p>
+                <p> Timezone: { res.timeZoneOffset } </p> 
+              </div> 
+              </div>);
             }) 
-          }</div>
+          }</div> */}
         </div>
         </div>
     );
