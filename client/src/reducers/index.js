a6c0d351b8faf6dc569147490ff07fbc2a0ce220
shania <shania.dhani46@cuny.edu>
2019-06-20 20:37:18 -0400

    confirm most up to date

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,6 @@
 // A barrel file for our reducers, which will be combined and passed into the Redux store we create;
 // The aliases of reducers in this file will be assigned as the names of the keys in the Redux store;
-export { default as ChatStore } from '../store/utilities/ChatStore';
+// export { default as ChatStore } from '../store/utilities/ChatStore';
 export { default as allTrips } from '../store/utilities/AllTrips';
 export { default as allUsers } from '../store/utilities/AllUsers';
 export { default as singleTrip } from '../store/utilities/SingleTrip';
