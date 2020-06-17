ee9a3a40b5875a08b6a4f8a94e801a87d34a5dd9
Shania <31454777+sdhani@users.noreply.github.com>
2020-06-17 17:02:16 -0400

    cleaned up code

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -6,14 +6,8 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import * as reducers from '../reducers';
 import ChatStore from './utilities/newChatStore'
 
-// console.log('Inside store', typeof ChatStore)
-// const ChatStore = (state = {messages: []}, action ) => {
-//     return state
-// }
-
 // Construct Redux Store;
 const rootReducer = combineReducers({...reducers, ChatStore});
-// const rootReducer = combineReducers(ChatStore)
 const logger = createLogger({collasped:true});
 const middleware = composeWithDevTools(applyMiddleware(thunkMiddleware, logger))
 const store = createStore(rootReducer, middleware);
